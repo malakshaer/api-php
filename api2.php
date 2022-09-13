e4f9d08850cfdd4366888c781754f58182e4581c
malakshaer <shaermk06@gmail.com>
2022-09-13 08:29:37 +0300

    modifing api 2 and 3

--- api2.php
+++ api2.php
@@ -5,16 +5,11 @@ $b = isset($_POST['b']);
 $c = isset($_POST['c']);
 
 if($b != 0){
-    $res = pow($a,3) + ($b*3) - ($a/$b);
+    echo (pow($a,3) + ($b*3) - ($a/$b));
 
 }else{
     echo "Error in second input";
 }
 
-$result =[
-    "result" => $res
-];
-
-echo json_encode($result);
 
 ?>
--- api3.php
+++ api3.php
@@ -7,14 +7,9 @@ $number    = preg_match('@[0-9]@', $password);
 $specialChars = preg_match('@[^\w]@', $password);
 
 if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
-    $res = "Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.";
+    echo "Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.";
 }else{
-    $res = "Strong password.";
+    echo "Strong password.";
 }
-
-$result =[
-    "res" => $res
-];
-echo json_encode($result);
 
 ?>
