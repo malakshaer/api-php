8153427c2eb5fbb27e369fa2db88bbf84deed896
malakshaer <shaermk06@gmail.com>
2022-09-13 08:32:40 +0300

    api 3 is modified

--- api3.php
+++ api3.php
@@ -6,7 +6,7 @@ $lowercase = preg_match('@[a-z]@', $password);
 $number    = preg_match('@[0-9]@', $password);
 $specialChars = preg_match('@[^\w]@', $password);
 
-if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 12) {
     echo "Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.";
 }else{
     echo "Strong password.";
