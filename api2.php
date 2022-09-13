e71c005b683e54b176afcb760ad14544c5ab7416
Malak Shaer <112479390+malakshaer@users.noreply.github.com>
2022-09-13 09:10:24 +0300

    Update api2.php
    change in line 8 variable c

--- api2.php
+++ api2.php
@@ -5,7 +5,7 @@ $b = isset($_POST['b']);
 $c = isset($_POST['c']);
 
 if($b != 0){
-    echo (pow($a,3) + ($b*3) - ($a/$b));
+    echo (pow($a,3) + ($b*$c) - ($a/$b));
 
 }else{
     echo "Error in second input";
